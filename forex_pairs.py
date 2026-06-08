e3d46dbd47e2c9e564af062293c59a72cd9aae40
mohsen202566 <norozi100296269@gmail.com>
2026-06-08 08:03:35 +0330

    Create forex_pairs.py

---
+++ forex_pairs.py
@@ -0,0 +1,23 @@
+FOREX_PAIRS = [
+    "EUR/USD",
+    "GBP/USD",
+    "USD/JPY",
+    "USD/CHF",
+    "AUD/USD",
+    "NZD/USD",
+    "USD/CAD",
+    "EUR/JPY",
+    "XAU/USD",
+]
+
+PAIR_DISPLAY_NAMES = {
+    "EUR/USD": "یورو / دلار",
+    "GBP/USD": "پوند / دلار",
+    "USD/JPY": "دلار / ین",
+    "USD/CHF": "دلار / فرانک",
+    "AUD/USD": "دلار استرالیا / دلار",
+    "NZD/USD": "دلار نیوزیلند / دلار",
+    "USD/CAD": "دلار / دلار کانادا",
+    "EUR/JPY": "یورو / ین",
+    "XAU/USD": "طلا / دلار",
+}
