926e2825d45c30e0ae84dd0fd086bcdc3493ec5a
mohsen202566 <norozi100296269@gmail.com>
2026-06-08 08:01:33 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+# Telegram
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+# Forex Pairs
+FOREX_PAIRS = [
+    "EUR/USD",
+    "GBP/USD",
+    "USD/JPY",
+    "USD/CHF",
+    "AUD/USD",
+    "NZD/USD",
+    "USD/CAD",
+    "EUR/JPY"
+]
+
+# Analysis Timeframes
+TREND_TF = "4h"
+CONFIRM_TF = "1h"
+SETUP_TF = "15min"
+ENTRY_TF = "5min"
+
+# Signal Settings
+MIN_SIGNAL_SCORE = 75
+BEST_SIGNAL_COUNT = 5
+
+# News Filter
+NEWS_BLOCK_BEFORE_MINUTES = 30
+NEWS_BLOCK_AFTER_MINUTES = 30
